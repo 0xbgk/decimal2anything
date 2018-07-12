b0dc7aa19c69940340ce4213d640616155d6a36c
BGK <gorkemkizilok@gmail.com>
2018-07-12 12:06:05 +0300

    form design, added textchanged function.

--- decimal2anything/Form1.Designer.cs
+++ decimal2anything/Form1.Designer.cs
@@ -35,29 +35,32 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // value
             // 
-            this.value.Dock = System.Windows.Forms.DockStyle.Left;
+            this.value.BackColor = System.Drawing.Color.Ivory;
+            this.value.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.value.Dock = System.Windows.Forms.DockStyle.Top;
             this.value.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.value.Location = new System.Drawing.Point(0, 0);
             this.value.Name = "value";
-            this.value.Size = new System.Drawing.Size(300, 32);
-            this.value.TabIndex = 0;
+            this.value.Size = new System.Drawing.Size(380, 32);
+            this.value.TabIndex = 1;
             this.value.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.value.TextChanged += new System.EventHandler(this.value_TextChanged);
             // 
             // convert_btn
             // 
-            this.convert_btn.BackColor = System.Drawing.Color.LightGreen;
-            this.convert_btn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.convert_btn.BackColor = System.Drawing.Color.Ivory;
             this.convert_btn.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
             this.convert_btn.FlatAppearance.BorderSize = 0;
             this.convert_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.convert_btn.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold);
-            this.convert_btn.Location = new System.Drawing.Point(469, 0);
+            this.convert_btn.Location = new System.Drawing.Point(466, 62);
             this.convert_btn.Name = "convert_btn";
             this.convert_btn.Size = new System.Drawing.Size(146, 33);
             this.convert_btn.TabIndex = 1;
@@ -67,11 +70,13 @@
             // 
             // sonuc
             // 
-            this.sonuc.Dock = System.Windows.Forms.DockStyle.Right;
+            this.sonuc.BackColor = System.Drawing.Color.Ivory;
+            this.sonuc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sonuc.Dock = System.Windows.Forms.DockStyle.Top;
             this.sonuc.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sonuc.Location = new System.Drawing.Point(315, 0);
+            this.sonuc.Location = new System.Drawing.Point(0, 32);
             this.sonuc.Name = "sonuc";
-            this.sonuc.Size = new System.Drawing.Size(300, 32);
+            this.sonuc.Size = new System.Drawing.Size(380, 32);
             this.sonuc.TabIndex = 2;
             this.sonuc.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -80,17 +85,17 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(615, 30);
+            this.panel1.Size = new System.Drawing.Size(461, 30);
             this.panel1.TabIndex = 3;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.convert_btn);
             this.panel2.Controls.Add(this.comboBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 75);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 151);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(615, 33);
+            this.panel2.Size = new System.Drawing.Size(461, 32);
             this.panel2.TabIndex = 4;
             // 
             // comboBox1
@@ -99,7 +104,8 @@
             "2",
             "8",
             "16"});
-            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.comboBox1.BackColor = System.Drawing.Color.Ivory;
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold);
             this.comboBox1.Items.AddRange(new object[] {
@@ -109,26 +115,38 @@
             this.comboBox1.Location = new System.Drawing.Point(0, 0);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(30, 3, 3, 3);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(130, 32);
-            this.comboBox1.TabIndex = 0;
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.comboBox1.Size = new System.Drawing.Size(461, 32);
+            this.comboBox1.TabIndex = 2;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.sonuc);
             this.panel3.Controls.Add(this.value);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 30);
+            this.panel3.Location = new System.Drawing.Point(0, 55);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(615, 45);
+            this.panel3.Size = new System.Drawing.Size(380, 69);
             this.panel3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(386, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(615, 108);
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(461, 183);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
@@ -139,6 +157,7 @@
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -151,6 +170,7 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- decimal2anything/Form1.cs
+++ decimal2anything/Form1.cs
@@ -18,146 +18,166 @@ namespace decimal2anything
             InitializeComponent();
             value.Text = "0";
             comboBox1.SelectedIndex = 0;
+            value.Focus();
+            label1.Text = "";
+            convert_btn.Visible = false;
             
         }
 
         public void convert()
         {
-            convertedtext = "";
-            int sayi = 0;
-            sayi = Convert.ToInt32(value.Text);
-            string def;
-            def = "";
-            if (comboBox1.SelectedIndex == 0)
+            try
             {
-                while (sayi > 0)
+                long sayi = 0;
+                convertedtext = "";
+                if (value.Text == "")
                 {
-                    if (sayi % 2 == 0)
-                    {
-                        def += "0";
-                    }
-                    else if (sayi % 2 == 1)
-                    {
-                        def += "1";
-                    }
-                    sayi = sayi / 2;
-                }
-            }
-            else if (comboBox1.SelectedIndex == 1)
-            {
-                while(sayi > 0)
-                {
-                    if (sayi % 8 == 0)
-                    {
-                        def += "0";
-                    }
-                    else if (sayi % 8 == 1)
-                    {
-                        def += "1";
-                    }
-                    else if (sayi % 8 == 2)
-                    {
-                        def += "2";
-                    }
-                    else if (sayi % 8 == 3)
-                    {
-                        def += "3";
-                    }
-                    else if (sayi % 8 == 4)
-                    {
-                        def += "4";
-                    }
-                    else if (sayi % 8 == 5)
-                    {
-                        def += "5";
-                    }
-                    else if (sayi % 8 == 6)
-                    {
-                        def += "6";
-                    }
-                    else if (sayi % 8 == 7)
-                    {
-                        def += "7";
-                    }
-                    sayi = sayi / 8;
-                }
-            }
-            else if (comboBox1.SelectedIndex == 2)
-            {
-                while (sayi > 0)
-                {
-                    if (sayi % 16 == 0)
-                    {
-                        def += "0";
-                    }
-                    if (sayi % 16 == 1)
-                    {
-                        def += "1";
-                    }
-                    if (sayi % 16 == 2)
-                    {
-                        def += "2";
-                    }
-                    if (sayi % 16 == 3)
-                    {
-                        def += "3";
-                    }
-                    if (sayi % 16 == 4)
-                    {
-                        def += "4";
-                    }
-                    if (sayi % 16 == 5)
-                    {
-                        def += "5";
-                    }
-                    if (sayi % 16 == 6)
-                    {
-                        def += "6";
-                    }
-                    if (sayi % 16 == 7)
-                    {
-                        def += "7";
-                    }
-                    if (sayi % 16 == 8)
-                    {
-                        def += "8";
-                    }
-                    if (sayi % 16 == 9)
-                    {
-                        def += "9";
-                    }
-                    if (sayi % 16 == 10)
-                    {
-                        def += "A";
-                    }
-                    if (sayi % 16 == 11)
-                    {
-                        def += "B";
-                    }
-                    if (sayi % 16 == 12)
-                    {
-                        def += "C";
-                    }
-                    if (sayi % 16 == 13)
-                    {
-                        def += "D";
-                    }
-                    if (sayi % 16 == 14)
-                    {
-                        def += "E";
-                    }
-                    if (sayi % 16 == 15)
-                    {
-                        def += "F";
-                    }
-                    sayi = sayi / 16;
-                }
-            }
 
-            for (int i = def.Length - 1; i >= 0; i--)
-            {
-                convertedtext += def[i];
+                }
+                else
+                {
+                    sayi = Convert.ToInt64(value.Text);
+                }
+
+                string def;
+                def = "";
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    while (sayi > 0)
+                    {
+                        if (sayi % 2 == 0)
+                        {
+                            def += "0";
+                        }
+                        else if (sayi % 2 == 1)
+                        {
+                            def += "1";
+                        }
+                        sayi = sayi / 2;
+                    }
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    while (sayi > 0)
+                    {
+                        if (sayi % 8 == 0)
+                        {
+                            def += "0";
+                        }
+                        else if (sayi % 8 == 1)
+                        {
+                            def += "1";
+                        }
+                        else if (sayi % 8 == 2)
+                        {
+                            def += "2";
+                        }
+                        else if (sayi % 8 == 3)
+                        {
+                            def += "3";
+                        }
+                        else if (sayi % 8 == 4)
+                        {
+                            def += "4";
+                        }
+                        else if (sayi % 8 == 5)
+                        {
+                            def += "5";
+                        }
+                        else if (sayi % 8 == 6)
+                        {
+                            def += "6";
+                        }
+                        else if (sayi % 8 == 7)
+                        {
+                            def += "7";
+                        }
+                        sayi = sayi / 8;
+                    }
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    while (sayi > 0)
+                    {
+                        if (sayi % 16 == 0)
+                        {
+                            def += "0";
+                        }
+                        if (sayi % 16 == 1)
+                        {
+                            def += "1";
+                        }
+                        if (sayi % 16 == 2)
+                        {
+                            def += "2";
+                        }
+                        if (sayi % 16 == 3)
+                        {
+                            def += "3";
+                        }
+                        if (sayi % 16 == 4)
+                        {
+                            def += "4";
+                        }
+                        if (sayi % 16 == 5)
+                        {
+                            def += "5";
+                        }
+                        if (sayi % 16 == 6)
+                        {
+                            def += "6";
+                        }
+                        if (sayi % 16 == 7)
+                        {
+                            def += "7";
+                        }
+                        if (sayi % 16 == 8)
+                        {
+                            def += "8";
+                        }
+                        if (sayi % 16 == 9)
+                        {
+                            def += "9";
+                        }
+                        if (sayi % 16 == 10)
+                        {
+                            def += "A";
+                        }
+                        if (sayi % 16 == 11)
+                        {
+                            def += "B";
+                        }
+                        if (sayi % 16 == 12)
+                        {
+                            def += "C";
+                        }
+                        if (sayi % 16 == 13)
+                        {
+                            def += "D";
+                        }
+                        if (sayi % 16 == 14)
+                        {
+                            def += "E";
+                        }
+                        if (sayi % 16 == 15)
+                        {
+                            def += "F";
+                        }
+                        sayi = sayi / 16;
+                    }
+                }
+
+                for (int i = def.Length - 1; i >= 0; i--)
+                {
+                    convertedtext += def[i];
+                }
+                label1.Text = def.Length.ToString()+" B.";
             }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            
         }
 
         private void convert_btn_Click(object sender, EventArgs e)
@@ -168,6 +188,13 @@ namespace decimal2anything
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            convert();
+            sonuc.Clear();
+            sonuc.Text = convertedtext;
+        }
+
+        private void value_TextChanged(object sender, EventArgs e)
         {
             convert();
             sonuc.Clear();
